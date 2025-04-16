267c1526966de9d7e8ce72a934fbdb959adbec20
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-04-16 20:00:14 -0300

    up: mudança na estrutura das pastas e add two numbers concluído

---
+++ README.md
@@ -0,0 +1,56 @@
+
+# 💡 Desafios de Lógica de Programação
+
+Este repositório reúne desafios de lógica resolvidos em **JavaScript**, **Python** e **Java**, organizados por **nível de dificuldade** e **plataforma** (LeetCode, Beecrowd, desafios próprios, etc).
+
+---
+
+## 📚 Níveis de Dificuldade
+
+- 🔰 **Iniciante** — Desafios básicos com foco em lógica fundamental e estruturas simples.
+
+- 🧠 **Intermediário** — Envolvem estruturas de dados (pilhas, filas, listas, árvores) e algoritmos mais elaborados.
+
+- 🧩 **Avançado** — Algoritmos complexos: grafos, programação dinâmica, técnicas de otimização, etc.
+
+---
+
+## 🗂 Estrutura do Projeto
+
+A estrutura do repositório segue a seguinte organização:
+
+desafios-logica-programacao/ 
+  ├── iniciante/  
+      ├── leetcode/  
+      ├── beecrowd/ 
+      ├── outros/ 
+  ├── intermediario/ 
+  ├── avancado/
+
+Cada desafio está em uma pasta com:
+
+- `descricao.txt` — Enunciado resumido ou traduzido.
+
+- Arquivos `.js`, `.py` e `.java` — Implementações nas linguagens disponíveis.
+
+---
+
+## Exemplos de Desafios
+
+### Iniciante
+- Two Sum
+- Add two Numbers
+
+### Intermediário
+
+### Avançado
+
+
+---
+
+## Como Usar
+
+Acesse a pasta de dificuldade e linguagem desejada, leia o enunciado no arquivo de descrição (**descricao.txt**) do código e execute com seu interpretador/compilador.
+
+---
+
---
+++ intermediario/leetcode/add_two_numbers/add_two_numbers.js
@@ -0,0 +1,72 @@
+
+// Definition for singly-linked list.
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var addTwoNumbers = function(l1, l2) {
+    let result = new ListNode(0, undefined);
+
+    let l = l1;
+    let r = l2;
+    let current = result;
+
+    let sobra = 0; 
+
+    while(l != null || r != null ){
+
+        let soma = current.val;
+
+        if(l != null){
+            soma += l.val; 
+            l = l.next;    
+        }
+        
+        if(r != null){
+            soma += r.val;
+            r = r.next;    
+        }
+
+        current.val = soma % 10;
+        sobra = parseInt(soma / 10);
+
+        if(!(r == null && l == null && sobra == 0)){
+            current.next = new ListNode(sobra, undefined); 
+            current = current.next;
+        }
+        
+
+    }
+
+    return result;
+
+};
+
+
+
+let l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+
+// Criando l2: 5 -> 6 -> 4
+let l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+
+// let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+
+// // Criando l2: 5 -> 6 -> 4
+// let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+
+let result = addTwoNumbers(l1, l2);
+
+// Mostrar resultado:
+while (result) {
+    console.log(result.val);  
+    result = result.next;
+}
