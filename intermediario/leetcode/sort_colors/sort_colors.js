ecba0f5df4a3728e09e653910776e87b6f3d553e
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-04-26 19:19:33 -0300

    up: problema sort colors em javaScript

---
+++ intermediario/leetcode/sort_colors/sort_colors.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    l = 0;
+    r = nums.length - 1;
+
+    while(l < r){
+
+
+        if(nums[l] != 2 && nums[l] != 1){
+            l+=1;
+        }
+
+        if(nums[r] != 0){
+            r-=1;
+        }
+        
+        if((nums[l] == 2 || nums[l] == 1) && nums[r] == 0 && l < r){
+            let aux = nums[l];
+            nums[l] = nums[r];
+            nums[r] = aux;
+            l+=1;
+            r-=1;
+        }
+    }
+
+    l = 0;
+    r = nums.length - 1;
+
+    while(l < r){
+
+        if(nums[l] != 2){
+            l+=1;
+        }
+
+        if(nums[r] != 1){
+            r-=1;
+        }
+
+        if(nums[l] == 2 && nums[r] == 1 && l < r){
+            let aux = nums[l];
+            nums[l] = nums[r];
+            nums[r] = aux;            
+            l+=1;
+            r-=1;       
+        }
+    }
+
+    return nums
+};
+        
+var nums = [0,1];
+
+console.log(sortColors(nums));
