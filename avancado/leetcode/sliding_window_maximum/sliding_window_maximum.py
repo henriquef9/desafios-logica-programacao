395d017c7f850de6a2a465289da5c23f28a808db
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-04-27 17:06:13 -0300

    up: problema resolvido com deque

--- avancado/leetcode/sliding_window_maximum/sliding_window_maximum.py
+++ avancado/leetcode/sliding_window_maximum/sliding_window_maximum.py
@@ -1,32 +1,72 @@
 import heapq
-# from queue import Queue
+from collections import deque
 
+# def maxSlidingWindow(nums, k):
+    
+#     maxHeap = [(-nums[i], i) for i in range(k)]
+
+#     heapq.heapify(maxHeap)
+    
+#     output = []
+#     startWindow = 0
+#     endWindow = k - 1
+
+#     while endWindow < len(nums):
+#         while True:
+#             maxVal, index = maxHeap[0]
+#             if index >= startWindow:
+#                 break
+#             heapq.heappop(maxHeap)
+        
+#         output.append(-maxVal)
+#         startWindow += 1
+#         endWindow += 1
+#         if endWindow < len(nums):
+#             heapq.heappush(maxHeap, (- nums[endWindow], endWindow))
+
+#     return output
+        
+    
 def maxSlidingWindow(nums, k):
-    
-    maxHeap = [(-nums[i], i) for i in range(k)]
 
-    heapq.heapify(maxHeap)
-    
+    fila = deque()
+
     output = []
     startWindow = 0
     endWindow = k - 1
 
-    while endWindow < len(nums):
-        while True:
-            maxVal, index = maxHeap[0]
-            if index >= startWindow:
-                break
-            heapq.heappop(maxHeap)
+
+    for i in range(0, k):
+
+        while fila and fila[-1][0] < nums[i]:
+            fila.pop()
+
+        fila.append((nums[i], i))
+    
+    output.append(fila[0][0])
+
+    if fila[0][1] == startWindow:
+        fila.popleft()
+
+    startWindow+=1
+    endWindow+=1
         
-        output.append(-maxVal)
-        startWindow += 1
-        endWindow += 1
-        if endWindow < len(nums):
-            heapq.heappush(maxHeap, (- nums[endWindow], endWindow))
+    while endWindow < len(nums):
+
+        while fila and fila[-1][0] < nums[endWindow]:
+            fila.pop()
+
+        fila.append((nums[endWindow], endWindow))
+
+        output.append(fila[0][0])
+
+        if fila[0][1] == startWindow:
+            fila.popleft()
+
+        startWindow+=1
+        endWindow+=1
 
     return output
-        
-    
 
 
 
