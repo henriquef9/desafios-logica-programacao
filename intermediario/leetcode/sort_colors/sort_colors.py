ec5c33f8cf49ccf3e2b64c918a200c850f66d5a5
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-06-20 15:09:24 -0300

    refactor: simplificar lógica de ordenação em sortColors

--- intermediario/leetcode/sort_colors/sort_colors.py
+++ intermediario/leetcode/sort_colors/sort_colors.py
@@ -1,19 +1,18 @@
 def sortColors(nums):
-    
     l = 0
     r = len(nums) - 1
 
     while l < r:
 
 
-        if nums[l] != 2 and nums[l] != 1:
+        if nums[l] == 0:
             l+=1
 
         if nums[r] != 0:
             r-=1
 
         
-        if (nums[l] == 2 or nums[l] == 1) and nums[r] == 0 and l < r:
+        if nums[l] != 0 and nums[r] == 0 and l < r:
             nums[l], nums[r] = nums[r], nums[l]
             l+=1
             r-=1
@@ -35,8 +34,11 @@ def sortColors(nums):
             r-=1        
     
     return nums
-        
-        
+    
+    
 nums = [0,1]
 
 print(sortColors(nums))
+
+    
+        
